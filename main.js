22df849ad2519ac56a1e1fdebc55a121fc6145bc
Abby Thoresen <akthoresen39@gmail.com>
2018-01-23 18:20:11 -0700

    some comments in main

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 // This will run the game. (node main.js)
 
+// Remember, to pretty-print objects when console.logging them in node:
+//`JSON.stringify(obj, null, 2)`
+
 var inquirer = require ("inquirer")
 
 // make fun colors
@@ -10,9 +13,31 @@ console.log('inverse the color'.inverse); // inverses the color
 console.log('OMG Rainbows!'.rainbow); // rainbow
 console.log('Run the trap'.trap); // Drops the bass
 
+var colors = require('colors');
+
+colors.setTheme({
+  silly: 'rainbow',
+  input: 'grey',
+  verbose: 'cyan',
+  prompt: 'grey',
+  info: 'green',
+  data: 'grey',
+  help: 'cyan',
+  warn: 'yellow',
+  debug: 'blue',
+  error: 'red'
+});
+
+// outputs red text
+console.log("this is an error".error);
+
+// outputs yellow text
+console.log("this is a warning".warn);
+
+
 // this is where the hangman game goes
 
-//1st step a word - pick a word.. user gursses letter - conversert word into spaces where each word correcspondes to a letter. each word is an object
+//1st step a word - pick a word.. user guesses letter - conversert word into spaces where each word correcspondes to a letter. each word is an object
 // one constructor word and other letters
 
 
